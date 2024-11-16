0d4a1a0d85a6af52e677c30842308c687a6064b6
hameedshk <kshameed7@gmail.com>
2024-11-16 20:19:51 +0530

    simpleflying css

--- components/homepage/ShortDescription.tsx
+++ components/homepage/ShortDescription.tsx
@@ -1,9 +1,9 @@
 const ShortDescription = () => {
   return (
     <div className="mb-4 mt-4">
-      <p>A Product owner based in Bangalore, India</p>
-      <p>As a Technical Product Manager with a proven foundation in full stack development, I bring a unique blend of technical expertise and strategic vision to bridge the gap between engineering teams and business objectives.</p>
-      <p>With hands-on experience in building scalable web applications, designing robust backend systems, and crafting user-centric frontends, I excel at translating complex technical challenges into actionable product solutions.</p>
+      <p>a Product owner based in Bangalore, India</p>
+      <p>I started learning to code in 2012 when I was in college.</p>
+      <p>I landed my first job as a Back-end Developer in 2014.</p>
       <p>I have a passion for for website development amd product management.</p>
       <p>I started this blog to rememebr my learnings, practice my skill and share my knowledge.</p>
     </div>
