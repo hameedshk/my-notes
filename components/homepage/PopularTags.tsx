50f2fad6ddea897742fd2f21aa9ce982f061cd60
hameedshk <kshameed7@gmail.com>
2024-11-17 11:56:47 +0530

    eslint fix

--- components/homepage/PopularTags.tsx
+++ components/homepage/PopularTags.tsx
@@ -9,7 +9,7 @@ const PopularTags = () => {
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div className="space-y-2 py-6 md:space-y-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 !mt-0">
+        <h1 className="!mt-0 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
           Popular Tags
         </h1>
         <p className="!mt-2 text-lg leading-7 text-gray-500 dark:text-gray-400">
