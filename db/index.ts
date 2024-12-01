54092d124db5f799970b3208539565412cdea31a
hameedshk <kshameed7@gmail.com>
2024-12-01 22:56:41 +0530

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 config({ path: '.env' })
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('process.env.DATABASE_URL is not set!')
+  // throw new Error('process.env.DATABASE_URL is not set!')
 }
 
 export let db = drizzle(process.env.DATABASE_URL)
