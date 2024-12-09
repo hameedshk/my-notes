4d87f6f515c7e3a060452015e874b37a4d89fc08
hameedshk <kshameed7@gmail.com>
2024-12-09 21:50:18 +0530

    change main menu icon to search icon

--- components/search/kbar-trigger.tsx
+++ components/search/kbar-trigger.tsx
@@ -7,11 +7,27 @@ export function KbarSearchTrigger() {
   return (
     <button
       aria-label="Search"
-      className="rounded p-1.5 hover:bg-gray-200 dark:hover:bg-gray-700"
+      // className="rounded p-1.5 hover:bg-gray-200 dark:hover:bg-gray-700"
       data-umami-event="search-the-site"
       onClick={() => query.toggle()}
     >
-      <Command size={20} strokeWidth={1.5} />
+     <span className="block w-5">
+                <svg
+                  className="text-gray-400 dark:text-gray-300"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </span>
+
     </button>
   )
 }
