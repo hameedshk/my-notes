b6fcd4f4399db6cd891d37a8f34e1e6b7dcdc9f6
hameedshk <kshameed7@gmail.com>
2024-12-10 14:44:58 +0530

    db connection & migration successfull

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,10 @@
 import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/postgres-js'
 
+
 config({ path: '.env' })
-if (!process.env.DATABASE_URL) {
+if (`${process.env.DATABASE_URL}`) {
   console.warn('process.env.DATABASE_URL is not set!')
 }
 
-export let db = drizzle(process.env.DATABASE_URL)
+export let db = drizzle(`${process.env.DATABASE_URL}`)
