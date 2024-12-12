d1268ba1bcb5c0617768fb3fed05af2065ac86d5
hameedshk <kshameed7@gmail.com>
2024-12-12 15:36:40 +0530

    get copyright year automatically

--- components/footer/footer-bottom.tsx
+++ components/footer/footer-bottom.tsx
@@ -6,6 +6,8 @@ import { SITE_METADATA } from '~/data/site-metadata'
 import MadeInIndia from '~/icons/miv.svg'
 import { Twemoji } from '../ui/twemoji'
 
+const fullYear = new Date().getFullYear();
+
 export function FooterBottom() {
   return (
     <div
@@ -21,27 +23,27 @@ export function FooterBottom() {
         showCover
       /> */}
       <div className="flex items-center">
-      <span className="mx-2.5 text-dark-400">SHK Inc © 2024</span>
-        <Link >        
-          <span data-umami-event="made-in-india">
+      <span className="mx-2.5 text-dark-400">SHK Inc © {fullYear}</span>
+        {/* <Link >         */}
+          {/* <span data-umami-event="made-in-india"> */}
             {/* <h1 className="absolute ml-1.5 inline-flex pt-[3px]">Made In India  */}
               <Twemoji emoji="flag-india" />
             {/* </h1> */}
-          </span>
-        </Link>       
+          {/* </span> */}
+        {/* </Link>        */}
         {/* <Github className="h-5 w-5" /> */}
         <span className="mx-2.5 text-dark-400">|</span>
         <Link href="/feed.xml" aria-label="RSS Feed" data-umami-event="rss-feed" prefetch={false}>
           <Rss strokeWidth={1.5} size={20} />
         </Link>
-        <Link
+        {/* <Link
           href={SITE_METADATA.analytics.umamiAnalytics.shareUrl}
           aria-label="Open analytics"
           data-umami-event="footer-analytics"
           className="ml-2"
-        >
+        > 
           <AreaChart strokeWidth={1.5} size={22} />
-        </Link> 
+        </Link> */}
       </div>
     </div>
   )
