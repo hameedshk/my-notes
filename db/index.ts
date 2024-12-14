0aeeb96cf975e52848d3c4b09bbf733724289423
hameedshk <kshameed7@gmail.com>
2024-12-14 12:34:28 +0530

    split connection string

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,20 @@
 import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/postgres-js'
-
+const { Client } = require('postgres')
+import bcrypt from 'bcryptjs'
+import { createCipheriv, createDecipheriv, randomBytes, scryptSync } from 'crypto';
 
 config({ path: '.env' })
-if (`${process.env.DATABASE_URL}`) {
-  console.warn('process.env.DATABASE_URL is not set!')
+
+if (!`${process.env.DATABASE_HOST}`) {
+  console.warn('database host is not set!')
 }
 
-export let db = drizzle(`${process.env.DATABASE_URL}`)
+const connectionString = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}?sslmode=require`;
+
+// const client = new Client({
+//   connectionString: connectionString,
+//   ssl: { rejectUnauthorized: false },
+// });
+
+export let db = drizzle(connectionString)
