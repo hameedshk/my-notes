9ebace0072d34af80a8740251c776f24dc9f768f
hameedshk <kshameed7@gmail.com>
2024-12-17 17:01:23 +0530

    css changes

--- components/author/social-accounts.tsx
+++ components/author/social-accounts.tsx
@@ -5,17 +5,7 @@ import X from '~/icons/x.svg'
 
 export function SocialAccounts({ className }: { className?: string }) {
   return (
-    <div className={clsx('flex items-center space-x-4', className)}>
-      <a
-        href={SITE_METADATA.github}
-        target="_blank"
-        data-umami-event="contact-github"
-        className="text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
-        rel="noopener noreferrer"
-      >
-        <span className="sr-only">Github</span>
-        <Github strokeWidth={1.5} />
-      </a>
+    <div className={clsx('flex items-center space-x-4', className)}>     
       {/* <a
         href={SITE_METADATA.x}
         target="_blank"
@@ -45,6 +35,16 @@ export function SocialAccounts({ className }: { className?: string }) {
       >
         <span className="sr-only">Mail</span>
         <Mail strokeWidth={1.5} />
+      </a>
+      <a
+        href={SITE_METADATA.github}
+        target="_blank"
+        data-umami-event="contact-github"
+        className="text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+        rel="noopener noreferrer"
+      >
+        <span className="sr-only">Github</span>
+        <Github strokeWidth={1.5} />
       </a>
       {/* <a
         href={SITE_METADATA.facebook}
--- components/home-page/index.tsx
+++ components/home-page/index.tsx
@@ -8,6 +8,9 @@ import { Intro } from './intro'
 import { LatestPosts } from './latest-posts'
 import { BlogLinks } from './links'
 import { TypedBios } from './typed-bios'
+import { Link } from '../ui/link'
+import { ProfileCardInfo } from '../cards/profile/profile-info'
+import { SocialAccounts } from '../author/social-accounts'
 
 export function Home({
   posts,
@@ -18,22 +21,29 @@ export function Home({
 }) {
   return (
     <Container as="div" className="pt-4 lg:pt-12">
-      <div className="md:pb-8 xl:grid xl:grid-cols-3">
-        <div className="space-y-4 md:space-y-6 md:pr-8 xl:col-span-2">
+      <div className="xl:grid xl:grid-cols-3">
+        <div className="space-y-6 md:space-y-8 md:pr-8 xl:col-span-2 text-left">
           <Greeting />
-          <div className="text-base leading-7 text-gray-600 dark:text-gray-400 md:text-lg md:leading-8">
+          <div className="text-base leading-7 text-gray-600 dark:text-gray-400 mt-1 md:text-lg md:leading-8">
              <Intro /> 
              <TypedBios />
-             <div className="mb-6 mt-4 md:mb-8">
-              <p>Welcome to My Notes</p>
-              <p>My Digital repository for Technology & Product Management Insights.</p>                            
-              <p>Unlocking Insights: My Journey in Tech and Product Management.</p>
-            </div> 
+             <div className="my-8">
+              <p className="font-semibold text-xl md:text-2xl">Welcome to My Notes</p>
+              <p className="mt-2 text-gray-500">My Digital repository for Technology & Product Management Insights.</p>                                          
+            </div>             
+            <div className="space-y-3 md:space-y-4">
             <BlogLinks />
-            <p className="my-6 flex md:my-8">
-              <span className="mr-2">Happy reading</span>
-              <Twemoji emoji="sparkles" />
-            </p>
+            </div>            
+            <div className="my-8">
+      <p className="text-lg font-medium">You can connect with me via</p>
+      <div className="flex justify-start space-x-6 mt-4">
+        <SocialAccounts />
+      </div>
+    </div>
+    <div className="my-8 text-center flex justify-start items-center">
+      <p className="text-lg font-medium mr-2">Happy reading</p>
+      <Twemoji emoji="sparkles" />
+    </div>
           </div>
         </div>
         <div className="hidden pl-4 xl:block">
